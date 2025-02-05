4c37f712b1b040dfe3aa98a9c595d32547d12289
Wo0zZ1 <Mper06@mail.ru>
2025-02-05 13:51:17 +0300

    feat: add memory logic, code improvement

---
+++ classes/Calculator.js
@@ -0,0 +1,31 @@
+class Calculator {
+	static calc(str) {
+		let temp = document.createElement('div')
+		temp.style['opacity'] = `calc((${str}))`
+		const result = parseFloat(
+			temp.style['opacity'].replace('calc(', '').replace(')', ''),
+		)
+		temp.remove()
+		return result
+	}
+
+	// static plus(x, y) {
+	// 	return x + y
+	// }
+
+	// static minus(x, y) {
+	// 	return x - y
+	// }
+
+	// static mult(x, y) {
+	// 	return x * y
+	// }
+
+	// static div(x, y) {
+	// 	return x / y
+	// }
+
+	// static pow(x, y) {
+	// 	return Math.pow(x, y)
+	// }
+}
---
+++ classes/Memory.js
@@ -0,0 +1,59 @@
+class Memory {
+	constructor() {
+		this.output = document.getElementById('output_data')
+
+		this.mc = document.getElementById('buttons_memory-btn-clear')
+		this.mr = document.getElementById('buttons_memory-btn-read')
+		this.mplus = document.getElementById('buttons_memory-btn-plus')
+		this.mminus = document.getElementById('buttons_memory-btn-minus')
+		this.ms = document.getElementById('buttons_memory-btn-set')
+
+		this.value = null
+
+		this.#bindEvents()
+	}
+
+	#createEvent(fn) {
+		return () => {
+			fn()
+			this.#render()
+			this.output.focus()
+		}
+	}
+
+	#bindEvents() {
+		this.mc.onclick = this.#createEvent(() => {
+			this.value = null
+		})
+
+		this.mr.onclick = this.#createEvent(() => {
+			if (this.value === null) return
+			this.output.value += this.value.toString()
+		})
+
+		this.mplus.onclick = this.#createEvent(() => {
+			if (!this.output.value.match(/^[0-9]+$/)) return
+			this.value = (this.value ?? 0) + parseFloat(this.output.value)
+		})
+
+		this.mminus.onclick = this.#createEvent(() => {
+			if (!this.output.value.match(/^[0-9]+$/)) return
+			this.value = (this.value ?? 0) - parseFloat(this.output.value)
+		})
+
+		this.ms.onclick = this.#createEvent(() => {
+			if (!this.output.value.match(/^[0-9]+$/)) return
+			this.value = parseFloat(this.output.value)
+		})
+	}
+
+	#render() {
+		if (this.value !== null) {
+			this.mc.classList.remove('disabled')
+			this.mr.classList.remove('disabled')
+		} else {
+			this.mc.classList.add('disabled')
+			this.mr.classList.add('disabled')
+		}
+	}
+}
